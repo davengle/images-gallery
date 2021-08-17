822fa0ecebe27ea6079cdf6685f211129a333b60
david.engle <davengle@gmail.com>
2021-08-17 13:53:13 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search'
+
 
 
 function App() {
+ 
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault()
+    console.log(searchTerm)
+  }
+
+
   return (
     <div className="App">
-      <Header title="Images Gallery 2"></Header>
+      <Header title="Images Gallery"></Header>
+      <Search searchTerm={ searchTerm } setSearchTerm={ setSearchTerm } handleSubmit={ handleSearchSubmit }/>
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,29 @@
+import react from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+const Search = ( {searchTerm, setSearchTerm, handleSubmit } ) => {
+    return (
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col xs={12} md= {8}>
+                    <Form onSubmit={ handleSubmit }>
+                        <Form.Row>
+                            <Col xs={9}>
+                                <Form.Control 
+                                    type="text"
+                                    value = { searchTerm }
+                                    onChange = { (e) => setSearchTerm(e.target.value) }
+                                    placeholder="Search for new image..."/>
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="Submit"> Search </Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Search;
